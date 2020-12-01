ff9912e3030b3837004897a718d1f6090ae65bd8
Tristan Deloche <tde@hey.com>
2020-12-01 23:54:48 +0000

    hashset d1 imple + biginput

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,10 +1,1 @@
-pub mod inputs {
-    use std::fs::File;
-    use std::io;
-    use std::io::BufRead;
-
-    pub fn read_lines(filename: &str) -> io::Result<io::Lines<io::BufReader<File>>> {
-        let file = File::open(filename)?;
-        Ok(io::BufReader::new(file).lines())
-    }
-}
+pub mod inputs {}
--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,62 +1,73 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
     println!("Day1!");
 
+    official();
+    biginput();
+}
+
+fn official() {
     let lines_iter = match File::open("input") {
         Ok(file) => BufReader::new(file).lines(),
         Err(err) => panic!(err),
     };
 
     let expenses = lines_iter
-        .map(|l| l.unwrap())
-        .map(|l| l.parse::<i64>().unwrap())
-        .collect::<Vec<i64>>();
+        .map(|l| l.unwrap().parse::<i64>().unwrap())
+        .collect::<HashSet<i64>>();
 
-    find_couple(&expenses);
-    find_trio(&expenses);
+    // part 1
+    let (p1v1, p1v2) = find2_n(2020, &expenses).unwrap();
+    println!("P1: {} * {} = {}", p1v1, p1v2, p1v1 * p1v2);
+
+    // part 2
+    let (p2v1, p2v2, p3v3) = expenses
+        .iter()
+        .find_map(|&v1| find2_n(2020 - v1, &expenses).map(|(v2, v3)| (v1, v2, v3)))
+        .unwrap();
+    println!(
+        "P2: {} * {} * {} = {}",
+        p2v1,
+        p2v2,
+        p3v3,
+        p2v1 * p2v2 * p3v3
+    );
 }
 
-fn find_couple(expenses: &Vec<i64>) {
-    let under_1010 = expenses
-        .iter()
-        .filter(|e| *e <= &1010)
-        .collect::<Vec<&i64>>();
+fn biginput() {
+    let biginput_sum = 99920044i64;
 
-    let over_1010 = expenses
-        .iter()
-        .filter(|e| *e > &1010)
-        .collect::<Vec<&i64>>();
+    let lines_iter_biginput = match File::open("input_big") {
+        Ok(big_input) => BufReader::new(big_input).lines(),
+        Err(err) => panic!(err),
+    };
+    let expenses_big = lines_iter_biginput
+        .map(|l| l.unwrap().parse::<i64>().unwrap())
+        .collect::<HashSet<i64>>();
 
-    for se in under_1010 {
-        let found = over_1010.iter().find(|&&be| *se + *be == 2020);
-        if found.is_some() {
-            let be = *found.unwrap();
-            let result = se * be;
-            println!("SE = {}, BE = {}", se, be);
-            println!("Code = {}", result);
-        }
-    }
+    // p1
+    let (bp1v1, bp1v2) = find2_n(biginput_sum, &expenses_big).unwrap();
+    println!("bP1: {} * {} = {}", bp1v1, bp1v2, bp1v1 * bp1v2);
+
+    // part 2
+    let (bp2v1, bp2v2, bp3v3) = expenses_big
+        .iter()
+        .find_map(|&v1| find2_n(biginput_sum - v1, &expenses_big).map(|(v2, v3)| (v1, v2, v3)))
+        .unwrap();
+    println!(
+        "P2: {} * {} * {} = {}",
+        bp2v1,
+        bp2v2,
+        bp3v3,
+        bp2v1 * bp2v2 * bp3v3
+    );
 }
 
-fn find_trio(expenses: &Vec<i64>) {
-    expenses.iter().for_each(|v1| -> () {
-        expenses
-            .iter()
-            .filter(|&v2| v1 + v2 <= 2020)
-            .for_each(|v2| -> () {
-                expenses
-                    .iter()
-                    .filter(|&v3| v1 + v2 + v3 <= 2020)
-                    .for_each(|v3| -> () {
-                        let result = v1 + v2 + v3;
-                        let code = v1 * v2 * v3;
-                        if result == 2020 {
-                            println!("Trio: {} + {} + {} = {}", v1, v2, v3, result);
-                            println!("{}", code);
-                        }
-                    })
-            })
-    });
+fn find2_n(n: i64, expenses: &HashSet<i64>) -> Option<(i64, i64)> {
+    return expenses
+        .iter()
+        .find_map(|&v1| expenses.get(&(n - v1)).map(|&v2| (v1, v2)));
 }
