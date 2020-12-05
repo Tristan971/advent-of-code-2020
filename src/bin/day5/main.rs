75f87b58b361abfe9055fe5ed3d69f5bcf712139
Tristan Deloche <tde@hey.com>
2020-12-05 06:31:33 +0000

    Use binary conversion

--- src/bin/day5/main.rs
+++ src/bin/day5/main.rs
@@ -1,49 +1,21 @@
-use std::collections::HashSet;
-use std::fmt::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-const ROWS: i32 = 128;
-const COLS: i32 = 7;
-
-struct Seat {
-    row: i32,
-    col: i32,
-    sid: i32,
-}
-
 fn main() {
-    let test_bp1 = read_seat(String::from("BFFFBBFRRR"));
-    println!("BP1: {} x {}", test_bp1.row, test_bp1.col);
-
-    let test_bp2 = read_seat(String::from("FFFBBBFRRR"));
-    println!("BP2: {} x {}", test_bp2.row, test_bp2.col);
-
-    let test_bp3 = read_seat(String::from("BBFFBBFRLL"));
-    println!("BP3: {} x {}", test_bp3.row, test_bp3.col);
-
-    let seats = File::open("src/bin/day5/input")
+    let mut sids = File::open("src/bin/day5/input")
         .map(BufReader::new)
         .map(|br| br.lines().map(|l| l.unwrap()))
         .unwrap()
-        .map(|line| read_seat(line))
-        .collect::<Vec<Seat>>();
+        .map(|line| to_sid(line))
+        .collect::<Vec<u32>>();
+    sids.sort_unstable();
 
-    let max_sid = seats.iter().map(|seat| seat.sid).max();
-    println!("Part 1: {}", max_sid.unwrap());
+    let max_sid = sids[sids.len() - 1];
+    println!("Part 1: {}", max_sid);
 
-    let mut contiguous_sids = seats
-        .iter()
-        .filter_map(|seat| match seat.row {
-            0 | ROWS => None,
-            _ => Some(seat.sid),
-        })
-        .collect::<Vec<i32>>();
-    contiguous_sids.sort_unstable();
-
-    for i in 1..(contiguous_sids.len()) {
-        let cur = contiguous_sids[i];
-        let prev = contiguous_sids[i - 1];
+    for i in 1..(sids.len()) {
+        let cur = sids[i];
+        let prev = sids[i - 1];
 
         let expected_cur = prev + 1;
         if cur != expected_cur {
@@ -53,36 +25,12 @@ fn main() {
     }
 }
 
-fn read_seat(boarding_pass: String) -> Seat {
-    let mut rowmin = 0;
-    let mut rowmax = ROWS;
-    let mut colmin = 0;
-    let mut colmax = COLS;
+fn to_sid(boarding_pass: String) -> u32 {
+    let bytes = boarding_pass
+        .replace("B", "1")
+        .replace("R", "1")
+        .replace("F", "0")
+        .replace("L", "0");
 
-    for char in boarding_pass.chars() {
-        let delta_row = (rowmax - rowmin) as f32 / 2.0; // 63.5
-        let delta_col = (colmax - colmin) as f32 / 2.0; // 3.5
-
-        match char {
-            'B' => {
-                rowmin += delta_row.ceil() as i32; // [0 + 64, 127]
-            }
-            'F' => {
-                rowmax -= delta_row.ceil() as i32; // [0, 127 - 64]
-            }
-            'R' => {
-                colmin += delta_col.ceil() as i32; // [0 + 4, 7]
-            }
-            'L' => {
-                colmax -= delta_col.ceil() as i32; // [0, 7 - 4]
-            }
-            _ => panic!("Invalid char: {}", char),
-        }
-    }
-
-    return Seat {
-        row: rowmin,
-        col: colmin,
-        sid: rowmin * 8 + colmin,
-    };
+    return u32::from_str_radix(bytes.as_str(), 2).unwrap();
 }
