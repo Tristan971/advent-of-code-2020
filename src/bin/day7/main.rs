8debc0520a94f8684ce7b8cd8592004b982475de
Tristan Deloche <tde@hey.com>
2020-12-07 06:42:06 +0000

    Filter day eh

---
+++ src/bin/day7/main.rs
@@ -0,0 +1,116 @@
+use std::collections::{HashMap, HashSet, VecDeque};
+
+use advent_of_code_2020::common::inputs;
+
+struct ContainedBag {
+    n: i32,
+    attrs: String,
+}
+
+struct Bag {
+    attrs: String,
+    contained_bags: Vec<ContainedBag>,
+}
+
+fn main() {
+    let bags = inputs::fread_lines("src/bin/day7/input")
+        .iter()
+        .map(|spec| to_bag(spec))
+        .collect::<Vec<Bag>>();
+
+    for bag in bags.iter() {
+        println!("Bag: {}", bag.attrs);
+        for contained_bag in bag.contained_bags.iter() {
+            println!("  {} {}", contained_bag.n, contained_bag.attrs)
+        }
+    }
+
+    let containers = to_containers(bags);
+
+    let containers_shiny_gold = containers_of(String::from("shiny gold"), &containers);
+    println!(
+        "Containers of shiny gold: {:#?} - {}",
+        containers_shiny_gold,
+        containers_shiny_gold.len()
+    );
+}
+
+fn containers_of(
+    attrs: String,
+    all_containers: &HashMap<String, HashSet<String>>,
+) -> HashSet<&str> {
+    let mut containers: HashSet<&str> = all_containers
+        .get(attrs.as_str())
+        .unwrap()
+        .iter()
+        .map(|c| c.as_str())
+        .collect();
+
+    let mut to_inspect: VecDeque<&str> = VecDeque::new();
+    for container in containers.clone() {
+        to_inspect.push_back(container);
+    }
+
+    while !to_inspect.is_empty() {
+        let parent = to_inspect.pop_front().unwrap();
+
+        all_containers.get(parent).unwrap().iter().for_each(|pc| {
+            containers.insert(pc);
+        })
+    }
+
+    return containers;
+}
+
+fn to_bag(spec: &str) -> Bag {
+    let parts = spec.split(" contain ").collect::<Vec<&str>>();
+
+    let container_attrs = parts[0].replace(" bags", "");
+
+    let contained_spec = parts[1];
+
+    // ending bags
+    if contained_spec.eq("no other bags.") {
+        return Bag {
+            attrs: container_attrs,
+            contained_bags: Vec::new(),
+        };
+    }
+
+    let _contained_bags = contained_spec
+        .replace(".", "")
+        .split(", ")
+        .map(|contained_bag| {
+            let bag_spec = contained_bag.replace(" bags", "").replace(" bag", "");
+            let index_spc = bag_spec.find(" ").unwrap();
+            let (n, spec) = bag_spec.split_at(index_spc);
+            let nb = n.parse::<i32>().unwrap();
+            let bag_spec = spec.trim_start();
+            return ContainedBag {
+                n: nb,
+                attrs: String::from(bag_spec),
+            };
+        })
+        .collect::<Vec<ContainedBag>>();
+
+    return Bag {
+        attrs: container_attrs,
+        contained_bags: _contained_bags,
+    };
+}
+
+fn to_containers(bags: Vec<Bag>) -> HashMap<String, HashSet<String>> {
+    let mut containers: HashMap<String, HashSet<String>> = HashMap::new();
+
+    for bag in bags.iter() {
+        for contained_bag in bag.contained_bags.iter() {
+            let cur_containers = containers
+                .entry(contained_bag.attrs.clone())
+                .or_insert_with(|| HashSet::new());
+
+            cur_containers.insert(bag.attrs.clone());
+        }
+    }
+
+    return containers;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,1 @@
-#[macro_use]
-extern crate lazy_static;
-
 pub mod common;
-pub mod passport;
